1572c9cd8c635904f639114dde95c3ce28af7345
NITESH <nmishra6115@gmail.com>
2026-05-23 23:36:34 +0530

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
+import { Analytics } from '@vercel/analytics/react';
+
+// Wrap your main app component with <Analytics />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Analytics />
+    <App />
+  </>
+);
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
