7cf898a143b73c0b04386fe49e90486b08806f52
NITESH <nmishra6115@gmail.com>
2026-05-26 14:51:47 +0530

    Updated website content

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// Using your URL directly here ensures it never crashes
+const supabaseUrl = 'https://qbketxxorhcaqpglgmaw.supabase.co';
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
