434a214c5ed51d0dd3c633abdc29829716f0c1b7
NITESH <nmishra6115@gmail.com>
2026-05-28 23:43:07 +0530

    Updated website content

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ export default function App() {
 <img 
       src="/logo.png" 
       alt="AML_DECODE Logo" 
-      className="h-16 w-auto" // Adjust height as needed
+      className="h-full w-auto object-contain" // Adjust height as needed
     />        <div className="hidden md:flex gap-6 items-center">
     {[ 
       { label: 'NOTES', id: 'notes' }, 
