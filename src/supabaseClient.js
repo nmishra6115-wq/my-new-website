bb223493631bd21c6f6941ac29d2b752c48525ff
NITESH <nmishra6115@gmail.com>
2026-05-28 13:14:17 +0530

    Updated website content

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-
 const supabaseUrl = 'https://qbketxxorhcaqpglgmaw.supabase.co';
-// Replace the string below with your REAL key from the dashboard
+// Use the key you copied from your Project Settings > API page
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFia2V0eHhvcmhjYXFwZ2xnbWF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3Nzg2NDEsImV4cCI6MjA5NTM1NDY0MX0.AqXhtIREnU7SuZ7c-liestHKn_DMp6BqP7aapMui7zw'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
