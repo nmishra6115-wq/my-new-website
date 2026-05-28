1d2f03157d0b3409bcf2bfcf5608e362aa2f002f
NITESH <nmishra6115@gmail.com>
2026-05-28 21:57:25 +0530

    Updated website content

--- src/jobs.js
+++ src/jobs.js
@@ -1,7 +1,10 @@
 export const jobOpenings = [
   // Bengaluru
   { company: "Revolut", role: "FinCrime Analyst (Investigator)", location: "Remote", link: "https://www.revolut.com/careers/position/fin-crime-analyst-investigator-672e3cb1-28d8-4aa7-8e4c-2149d9b8c82b/" },
-  { company: "Wells Fargo", role: "Due Diligence Associate", location: "Bengaluru", link: "https://www.wellsfargojobs.com/en/jobs/r-529171/due-diligence-associate/" },
+  { company: "Societe Generale Global Solution Centre", role: "Senior Analyst ", location: "Bengaluru", link: "https://careers.societegenerale.com/en/job-offers/senior-analyst-26000C11-en?src=JB-14381" },
+    { company: "IBM", role: "PROCESS DELIVERY SPECIALIST-RISK & COMPLIANCE ", location: "Bengaluru", link: "https://careers.ibm.com/en_US/careers/JobDetail?jobId=117131&source=SN_LinkedIn" },
+    { company: "Accenture ", role: " Risk and Compliance Associate ", location: "Bengaluru", link: "https://www.accenture.com/in-en/careers/jobdetails?id=R00330824_en&src=LINKEDINJP" },
+
   { company: "JPMorgan Chase", role: "Compliance Risk Mgmt Sr Associate", location: "Bengaluru", link: "https://jpmc.fa.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1001/jobs/preview/210744073/apply/email?keyword=Compliance&latitude=12.953378788038888&location=4th+Cross+Road%2C+Bengaluru%2C+KA%2C+India&longitude=77.70340361368119&mode=geolocation&radius=25&radiusUnit=MI" },
    { company: "Skydo", role: "Compliance Operations & Risk Analyst - AML/KYC (Fintech)", location: " Bengaluru", link: "https://app.fabrichq.ai/jobs/4a4732e1-6886-41f1-bfd0-92b97eb05da0" },
   // Mumbai
