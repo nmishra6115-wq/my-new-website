de549a8e066dc40c7059e1f6a760e0e55b9cf308
NITESH <nmishra6115@gmail.com>
2026-06-03 12:00:19 +0530

    Updated website content and deployed alerts

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-client';
 
-const supabaseUrl = 'https://qbketxxorhcaqpglgmaw.supabase.co';
-// Use the key you copied from your Project Settings > API page
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFia2V0eHhvcmhjYXFwZ2xnbWF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3Nzg2NDEsImV4cCI6MjA5NTM1NDY0MX0.AqXhtIREnU7SuZ7c-liestHKn_DMp6BqP7aapMui7zw'; 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
